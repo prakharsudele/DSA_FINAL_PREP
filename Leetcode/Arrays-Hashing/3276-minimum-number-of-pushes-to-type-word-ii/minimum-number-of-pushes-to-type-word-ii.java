f892c0a919ba04b7dd9701a5e39e6d693548d0f3
prakharsudele <prakharsudele36@gmail.com>
2026-07-31 08:20:01 +0530

    Time: 9 ms (94.22%) | Memory: 48.3 MB (17.34%) - LeetSync

--- Leetcode/Arrays-Hashing/3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
+++ Leetcode/Arrays-Hashing/3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
@@ -1,59 +1,18 @@
 class Solution {
     public int minimumPushes(String word) {
+
         int[] freq = new int[26];
-        HashSet<Character> set = new HashSet<>();
-        for(char ch : word.toCharArray()){
-            set.add(ch);
+        for (char ch : word.toCharArray()) {
             freq[ch - 'a']++;
         }
-
+        Arrays.sort(freq);
         int ans = 0;
         int count = 0;
-        int cross = set.size();
-        Arrays.sort(freq);
-        if(cross <= 8){
-            for(int i=25 ;i>=0;i--){
-                if(freq[i]!=0) ans+=freq[i];
-            }
-        }else if(cross <= 16){
-            for(int i=25 ;i>=0;i--){
-                if(freq[i]!=0 && count<8){
-                    ans+=freq[i];
-                    count++;
-                }else if(freq[i] != 0){
-                    ans+=freq[i]*2;
-                    count++;
-                }
-            }
-        }else if(cross<= 24){
-            for(int i=25 ;i>=0;i--){
-                if(freq[i]!=0 && count<8){
-                    ans+=freq[i];
-                    count++;
-                }else if(freq[i] != 0 && count<16){
-                    ans+=freq[i]*2;
-                    count++;
-                }else if(freq[i] != 0){
-                    ans+=freq[i]*3;
-                    count++;
-                }
-            }
-        }else{
-            for(int i=25 ;i>=0;i--){
-                if(freq[i]!=0 && count<8){
-                    ans+=freq[i];
-                    count++;
-                }else if(freq[i] != 0 && count<16){
-                    ans+=freq[i]*2;
-                    count++;
-                }else if(freq[i] != 0 && count<24){
-                    ans+=freq[i]*3;
-                    count++;
-                }else if(freq[i]!=0){
-                    ans+=freq[i]*4;
-                    count++;
-                }
-            }
+        for (int i = 25; i >= 0; i--) {
+            if (freq[i] == 0) continue;
+
+            ans += freq[i] * (count / 8 + 1);
+            count++;
         }
         return ans;
     }
