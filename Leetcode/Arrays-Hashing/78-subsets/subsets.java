6f7fe8524710ad7eef9165d475d212f4e518c64e
prakharsudele <prakharsudele36@gmail.com>
2026-08-06 07:57:20 +0530

    Time: 1 ms (86.72%) | Memory: 43.9 MB (88.96%) - LeetSync

--- Leetcode/Arrays-Hashing/78-subsets/subsets.java
+++ Leetcode/Arrays-Hashing/78-subsets/subsets.java
@@ -1,24 +1,24 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> ds = new ArrayList<>();
+        List<Integer> ds =  new ArrayList<>();
 
-        subset(ans , ds , 0 , nums);
+        find(ans , ds , 0 , nums);
         return ans;
     }
-
-    private void subset(List<List<Integer>> ans , List<Integer> ds , int index , int[] nums){
+    private void find(List<List<Integer>> ans , List<Integer> ds , int index , int[] nums){
+        //base case (when go back)
         if(index == nums.length){
             ans.add(new ArrayList<>(ds));
             return;
         }
 
-        //take 
+        //choose this element 
         ds.add(nums[index]);
-        subset(ans , ds , index+1 , nums);
-        ds.remove(ds.size()-1);
+        find(ans , ds, index+1 , nums);
 
-        //not take
-        subset(ans , ds , index+1 , nums);
+        //don't choose this element
+        ds.remove(ds.size() - 1);
+        find(ans , ds , index+1 , nums); 
     }
 }
