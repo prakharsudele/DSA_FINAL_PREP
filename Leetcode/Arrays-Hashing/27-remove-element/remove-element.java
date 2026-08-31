d43c88be846b1edd387ed046860ceac51945d3cb
prakharsudele <prakharsudele36@gmail.com>
2026-08-31 09:23:12 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (61.52%) - LeetSync

--- Leetcode/Arrays-Hashing/27-remove-element/remove-element.java
+++ Leetcode/Arrays-Hashing/27-remove-element/remove-element.java
@@ -1,20 +1,16 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int i = 0;
-        int j = 0;
-        int count  = 0;
-        while(j < nums.length){
-            if(nums[j] != val){
-                count++;
-                int temp = nums[j];
-                nums[j] = nums[i];
-                nums[i] = temp;
-                i++;
-                j++;
-            }else{
-                j++;
+        int count = 0;
+        for(int it : nums) if(it == val) count++;
+
+        int index = 0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] != val){
+                nums[index] = nums[i];
+                index++;
             }
         }
-        return count;
+
+        return nums.length - count;
     }
 }
