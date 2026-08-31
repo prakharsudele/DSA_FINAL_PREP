5b8fcbe3a1ef3ede492b4df4fd5263feb94c73ff
prakharsudele <prakharsudele36@gmail.com>
2026-08-31 09:33:24 +0530

    Time: 1 ms (77.68%) | Memory: 46.6 MB (63.37%) - LeetSync

--- Leetcode/Arrays-Hashing/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ Leetcode/Arrays-Hashing/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -1,17 +1,20 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int curr = -10000;
+        int curr = -10000; // OUT OF THE RANGE
         int index = 0;
 
         for(int i=0;i<nums.length;i++){
-            if(nums[i] != curr){
+            if(nums[i] != curr){ // NEW ELEMENT
                 nums[index] = nums[i];
                 curr = nums[i];
                 index++;
             }
 
-            if(curr == -10000) curr = nums[i]; 
+            if(curr == -10000) curr = nums[i]; //UPDATE CURR FIRST TIME
         }
         return index;
     }
 }
+
+//TC -->  O(N)
+// SC --> O(1)
