b297bfafbfc5a4632a3d6f1b3a96e3a4f26ea8d7
prakharsudele <prakharsudele36@gmail.com>
2026-09-01 09:26:38 +0530

    Time: 2 ms (93.37%) | Memory: 71.6 MB (64.85%) - LeetSync

--- Leetcode/Arrays-Hashing/238-product-of-array-except-self/product-of-array-except-self.java
+++ Leetcode/Arrays-Hashing/238-product-of-array-except-self/product-of-array-except-self.java
@@ -2,6 +2,10 @@ class Solution {
     public int[] productExceptSelf(int[] nums) {
         //brute force could be check for every element TC --> O(N^2) SC --> O(1)
 
+        //optimal solution is firgure out the complete mul of numns ans divide by nums[i] in every turn 
+        // EDGE CASE : nums[i] == 0 zero division not defined if array contains more than 1 zero ans will always be 0 if less than 1 or equalt o 1 check if curr is zero or not
+        
+
         boolean zero = false;
         int count = 0;
         int prod = 1;
@@ -24,3 +28,6 @@ class Solution {
         return nums;
     }
 }
+
+//SC --> O(N)
+//TC --> O(1) as we are updating nums itself.
