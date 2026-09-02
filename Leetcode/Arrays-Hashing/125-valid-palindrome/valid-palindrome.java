161ccf4ddaf29ccc8e76380db67f24ead19d6718
prakharsudele <prakharsudele36@gmail.com>
2026-09-02 12:22:41 +0530

    Time: 4 ms (44.66%) | Memory: 44.6 MB (44.35%) - LeetSync

--- Leetcode/Arrays-Hashing/125-valid-palindrome/valid-palindrome.java
+++ Leetcode/Arrays-Hashing/125-valid-palindrome/valid-palindrome.java
@@ -1,20 +1,25 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        StringBuilder newStr = new StringBuilder();
-        for (char i : s.toCharArray()) {
-            if (Character.isLetterOrDigit(i)) {
-                newStr.append(Character.toLowerCase(i));
+        //we are going to create an stringbuilder with no spaces and lower case character only afterwards we will check if it's an plaidrome with help of 2 pointers.
+
+        int n = s.length();
+        StringBuilder sb = new StringBuilder();
+        for(char ch : s.toCharArray()){
+            if(Character.isLetterOrDigit(ch)){ //check if it's alphanumeric character
+                sb.append(Character.toLowerCase(ch));//add it in lowercase
             }
         }
 
-        int i = 0;
-        int j = newStr.length() - 1;
-
+        int i=0,j=sb.length()-1;
         while(i < j){
-            if(newStr.charAt(i) != newStr.charAt(j)) return false;
+            if(sb.charAt(i) != sb.charAt(j)) return false; //check for palidrome
             i++;
             j--;
         }
+
         return true;
     }
 }
+
+//TC --> O(N)
+//SC --> O(1)
