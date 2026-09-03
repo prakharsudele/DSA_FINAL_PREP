0bd14eb6d8d698d0c4016e64263d44f7ef63f975
prakharsudele <prakharsudele36@gmail.com>
2026-09-03 09:01:06 +0530

    Time: 2291 ms (5.00%) | Memory: 65.4 MB (5.26%) - LeetSync

--- Leetcode/Arrays-Hashing/15-3sum/3sum.java
+++ Leetcode/Arrays-Hashing/15-3sum/3sum.java
@@ -4,13 +4,13 @@ class Solution {
         //run 3 nested loops to figure out 3 numbers that will be equal to target = 0.
         //TC-->O(N^2) (3*10^3)3 = 10^9 very slow won't accept.
 
-        Arrays.sort(nums);
+        Arrays.sort(nums); //need to sort in order to get only unique values.
         List<List<Integer>> ans = new ArrayList<>();
-        HashSet<List<Integer>> set = new HashSet<>();
+        HashSet<List<Integer>> set = new HashSet<>();//to check if we already have this list.
 
         for(int i=0;i<nums.length;i++){
             int target = nums[i];
-            target = -target;
+            target = -target;//new target if other 2 are equal to this then sum is equal to 0.
             HashMap<Integer , Integer> map = new HashMap<>();
             for(int j=i+1;j<nums.length;j++){
                 if(map.containsKey(nums[j]) && !set.contains(Arrays.asList(nums[i] , target-nums[j] , nums[j]))){
@@ -20,8 +20,10 @@ class Solution {
                 map.put(target - nums[j] , j);
             }
         }
-
-
         return ans;
     }
 }
+
+
+//TC --> O(N^2)
+//SC --> O(1)
