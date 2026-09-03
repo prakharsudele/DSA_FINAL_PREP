a05086a3e87a071c48cfce506891bf5231e9c9cb
prakharsudele <prakharsudele36@gmail.com>
2026-09-03 08:28:05 +0530

    Time: 48 ms (8.61%) | Memory: 46.9 MB (74.49%) - LeetSync

--- Leetcode/Arrays-Hashing/1-two-sum/two-sum.java
+++ Leetcode/Arrays-Hashing/1-two-sum/two-sum.java
@@ -1,13 +1,15 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int n = nums.length;
-        HashMap<Integer , Integer> map = new HashMap<>();
+        //BRUTE FORCE -->
+        //we can start from every ith index and search in array to find if ith + jth will add up to target or not but it will take -
+        //O(N^2) Might work for this problem seeing constraints but optimally we have to do this in one pass.
 
-        for(int i = 0 ; i < n ; i++){
-            if(map.containsKey(nums[i])) return new int[]{map.get(nums[i]) , i};
-            map.put(target - nums[i] , i);
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i] + nums[j] == target) return new int[]{i , j};
+            }
         }
 
-        return new int[]{};
+        return new int[]{-1 , -1};
     }
 }
