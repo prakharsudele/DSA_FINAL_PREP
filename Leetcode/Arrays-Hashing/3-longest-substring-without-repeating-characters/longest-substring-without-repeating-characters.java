c8cfa27f5026bde0933dec9e8200b6ce3104857b
prakharsudele <prakharsudele36@gmail.com>
2026-09-03 10:58:30 +0530

    Time: 41 ms (46.27%) | Memory: 47.9 MB (24.54%) - LeetSync

--- Leetcode/Arrays-Hashing/3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ Leetcode/Arrays-Hashing/3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -5,17 +5,24 @@ class Solution {
         //TC-->O(N^2)
         //SC -->O(N)
 
+        //OPTIMAL SOLUTION 
+        //loop in one pass and keep checking if this character is not already in our map if it is then simply take shrink our window take it to last time we saw this index + 1.
+        //keep in mind to not traceback so take max of already where we are and index+1.
+
         HashMap<Character , Integer> map = new HashMap<>();
         int maxLength = 0;
         int i=0;
-        for(int j=0;j<s.length();j++){
+        for(int j=0;j<s.length();j++){//expand window
             if(map.containsKey(s.charAt(j))){
-                i = Math.max(i , map.get(s.charAt(j)) + 1);
+                i = Math.max(i , map.get(s.charAt(j)) + 1); //shrink window
             }
 
-            map.put(s.charAt(j) , j);
+            map.put(s.charAt(j) , j); 
             maxLength = Math.max(maxLength , j-i+1);
         }
         return maxLength;
     }
 }
+
+//TC --> O(N)
+//SC --> O(N)
