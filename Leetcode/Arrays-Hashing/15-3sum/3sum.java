f08046f7b9eccc50ad071b849fa791d3248e393f
prakharsudele <prakharsudele36@gmail.com>
2026-09-03 09:06:35 +0530

    Time: 28 ms (96.67%) | Memory: 59.1 MB (74.60%) - LeetSync

--- Leetcode/Arrays-Hashing/15-3sum/3sum.java
+++ Leetcode/Arrays-Hashing/15-3sum/3sum.java
@@ -2,28 +2,40 @@ class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         //BRUTE FORCE 
         //run 3 nested loops to figure out 3 numbers that will be equal to target = 0.
-        //TC-->O(N^2) (3*10^3)3 = 10^9 very slow won't accept.
+        //TC-->O(N^3) (3*10^3)3 = 10^9 very slow won't accept.
 
-        Arrays.sort(nums); //need to sort in order to get only unique values.
-        List<List<Integer>> ans = new ArrayList<>();
-        HashSet<List<Integer>> set = new HashSet<>();//to check if we already have this list.
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);//need to sort in order to get only unique values. (if sorted then question turns into 2sum with sorted array no need of hashmap and hashset)
 
-        for(int i=0;i<nums.length;i++){
-            int target = nums[i];
-            target = -target;//new target if other 2 are equal to this then sum is equal to 0.
-            HashMap<Integer , Integer> map = new HashMap<>();
-            for(int j=i+1;j<nums.length;j++){
-                if(map.containsKey(nums[j]) && !set.contains(Arrays.asList(nums[i] , target-nums[j] , nums[j]))){
-                    ans.add(Arrays.asList(nums[i] , target-nums[j] , nums[j]));
-                    set.add(Arrays.asList(nums[i] , target-nums[j] , nums[j]));
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i-1]) { //skip duplicates
+                continue;
+            }
+            
+            int j = i + 1;
+            int k = nums.length - 1;
+
+            while (j < k) {
+                int total = nums[i] + nums[j] + nums[k];
+
+                if (total > 0) {
+                    k--;
+                } else if (total < 0) {
+                    j++;
+                } else {
+                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+
+                    while (nums[j] == nums[j-1] && j < k) {//skip duplicates
+                        j++;
+                    }
                 }
-                map.put(target - nums[j] , j);
             }
         }
-        return ans;
+        return res;
     }
 }
 
 
 //TC --> O(N^2)
-//SC --> O(1)
+//SC --> O(N)
