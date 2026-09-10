d2dcd9705372195d1bb51082ef839041acbb0488
prakharsudele <prakharsudele36@gmail.com>
2026-09-10 09:53:24 +0530

    Time: 9 ms (34.57%) | Memory: 48.9 MB (85.04%) - LeetSync

---
+++ Leetcode/Arrays-Hashing/56-merge-intervals/merge-intervals.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[][] merge(int[][] A) {
+        int n = A.length;
+        Arrays.sort(A , (a,b) -> Integer.compare(a[0] , b[0]));
+        List<List<Integer>> ans = new ArrayList<>();
+        int startNow = A[0][0];
+        int endNow = A[0][1];
+        for(int i=1;i<n;i++){
+            int startNext = A[i][0];
+            int endNext = A[i][1];
+
+            if(endNow >= startNext){
+                endNow = Math.max(endNow , endNext);
+            }else{
+                ans.add(Arrays.asList(startNow, endNow));
+                startNow = startNext;
+                endNow = endNext;
+            }
+        }
+
+        ans.add(Arrays.asList(startNow , endNow));
+
+        int[][] result = new int[ans.size()][2];
+
+        for(int i = 0; i < ans.size(); i++) {
+            result[i][0] = ans.get(i).get(0);
+            result[i][1] = ans.get(i).get(1);
+        }
+
+        return result;
+    }
+}
