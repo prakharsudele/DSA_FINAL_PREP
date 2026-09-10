dbcc70896faae7a3513c37d7366d0fb503bbb882
prakharsudele <prakharsudele36@gmail.com>
2026-09-10 09:55:51 +0530

    Time: 9 ms (34.57%) | Memory: 48.8 MB (91.92%) - LeetSync

--- Leetcode/Arrays-Hashing/56-merge-intervals/merge-intervals.java
+++ Leetcode/Arrays-Hashing/56-merge-intervals/merge-intervals.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[][] merge(int[][] A) {
         int n = A.length;
-        Arrays.sort(A , (a,b) -> Integer.compare(a[0] , b[0]));
+        Arrays.sort(A , (a,b) -> Integer.compare(a[0] , b[0]));//sort by starting value
         List<List<Integer>> ans = new ArrayList<>();
         int startNow = A[0][0];
         int endNow = A[0][1];
@@ -9,18 +9,18 @@ class Solution {
             int startNext = A[i][0];
             int endNext = A[i][1];
 
-            if(endNow >= startNext){
+            if(endNow >= startNext){ // if overlap
                 endNow = Math.max(endNow , endNext);
-            }else{
+            }else{ //no overlap simply add in list
                 ans.add(Arrays.asList(startNow, endNow));
                 startNow = startNext;
                 endNow = endNext;
             }
         }
 
-        ans.add(Arrays.asList(startNow , endNow));
+        ans.add(Arrays.asList(startNow , endNow));//last timeline inserted.
 
-        int[][] result = new int[ans.size()][2];
+        int[][] result = new int[ans.size()][2];//covert it in array from list.
 
         for(int i = 0; i < ans.size(); i++) {
             result[i][0] = ans.get(i).get(0);
@@ -30,3 +30,6 @@ class Solution {
         return result;
     }
 }
+
+//TC --> O(nlogn)
+//SC --> O(1)
