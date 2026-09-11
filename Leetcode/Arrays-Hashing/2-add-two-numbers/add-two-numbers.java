ffc83316e41f398704e8bd5339f6e8cab197b43d
prakharsudele <prakharsudele36@gmail.com>
2026-09-11 08:32:37 +0530

    Time: 1 ms (100.00%) | Memory: 46.6 MB (19.61%) - LeetSync

--- Leetcode/Arrays-Hashing/2-add-two-numbers/add-two-numbers.java
+++ Leetcode/Arrays-Hashing/2-add-two-numbers/add-two-numbers.java
@@ -10,31 +10,31 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode news = null;
+        ListNode news = null; //new linkedlist to store result.
         ListNode temp = news;
         ListNode curr1 = l1;
         ListNode curr2 = l2;
-        int carry = 0;
+        int carry = 0; //keep track of carry.
 
         while (curr1 != null && curr2 != null) {
             int one = curr1.val;
             int two = curr2.val;
             int sum = one + two + carry;
             if(temp == null) {
-                temp = new ListNode(sum % 10);
-                news = temp;
+                temp = new ListNode(sum % 10); //create node and add val.
+                news = temp;//intialize news to temp.
             } else {
                 news.next = new ListNode(sum % 10);
                 news = news.next;
             }
 
-            carry = sum/10;
+            carry = sum/10; //carry update
 
             curr1 = curr1.next;
             curr2 = curr2.next;
         }
 
-        while (curr1 != null) {
+        while (curr1 != null) { //if there are values left in l1.
             int one = curr1.val;
             int sum = one + carry;
             if (sum > 9) {
@@ -50,7 +50,7 @@ class Solution {
             curr1 = curr1.next;
         }
 
-        while (curr2 != null) {
+        while (curr2 != null) { //if there are value left in l2.
             int one = curr2.val;
             int sum = one + carry;
             if (sum > 9) {
@@ -66,10 +66,13 @@ class Solution {
             curr2 = curr2.next;
         }
 
-        if(carry > 0){
+        if(carry > 0){ //if in the end we have any carry add that too.
             news.next = new ListNode(carry);
         }
 
         return temp;
     }
 }
+
+//TC --> O(N)
+//SC --> 0(1)
