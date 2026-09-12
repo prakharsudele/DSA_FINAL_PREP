bf8b06acdeecf68c2329e46f59f447fbc60c84ba
prakharsudele <prakharsudele36@gmail.com>
2026-09-12 08:54:48 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (19.82%) - LeetSync

--- Leetcode/Arrays-Hashing/21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ Leetcode/Arrays-Hashing/21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -10,12 +10,12 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode dummy = new ListNode();
+        ListNode dummy = new ListNode(); //new list
         ListNode curr = dummy;
         if(list1 == null && list2 == null) return dummy.next;
 
         while(list1 != null && list2 != null){
-            if(list1.val < list2.val){
+            if(list1.val < list2.val){ //compare 2 values and update accordingly.
                 curr.next = new ListNode(list1.val);
                 list1 = list1.next;
             }else{
@@ -23,10 +23,13 @@ class Solution {
                 list2 = list2.next;
             }
 
-            curr = curr.next;
+            curr = curr.next; //update curr everytime.
         }
 
-        curr.next = (list1 != null) ? list1 : list2;
+        curr.next = (list1 != null) ? list1 : list2; //add last element if list1 is not null so list1 else list2.
         return dummy.next;
     }
 }
+
+//TC --> O(N)
+//SC --> O(1) //returned list not included.
