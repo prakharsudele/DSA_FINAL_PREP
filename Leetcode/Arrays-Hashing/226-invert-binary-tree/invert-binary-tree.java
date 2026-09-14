b3a699d29fedfdac1d9ded88910ff11d177400f6
prakharsudele <prakharsudele36@gmail.com>
2026-09-14 13:56:08 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (46.15%) - LeetSync

--- Leetcode/Arrays-Hashing/226-invert-binary-tree/invert-binary-tree.java
+++ Leetcode/Arrays-Hashing/226-invert-binary-tree/invert-binary-tree.java
@@ -1,16 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public TreeNode invertTree(TreeNode root) {
-        if (root == null) {
-            return null;
-        }
-        
+        if(root == null) return null;
+
+        TreeNode temp = root.left; //store in temp node.
+        root.left = root.right;//exchange subtrees.
+        root.right = temp;
+
+        //exchange right and left subtree for every node.
         invertTree(root.left);
         invertTree(root.right);
-
-        TreeNode temp = root.left;
-        root.left = root.right;
-        root.right = temp;   
-
-        return root; 
+        return root;
     }
 }
+
+//TC --> O(M) no of nodes.
+//SC --> O(H) height of tree 
