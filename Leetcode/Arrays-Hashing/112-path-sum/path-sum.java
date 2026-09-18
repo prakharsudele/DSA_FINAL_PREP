428e3ebe687b2cd747e8b89152aa73705daf8301
prakharsudele <prakharsudele36@gmail.com>
2026-09-18 16:54:52 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (49.61%) - LeetSync

--- Leetcode/Arrays-Hashing/112-path-sum/path-sum.java
+++ Leetcode/Arrays-Hashing/112-path-sum/path-sum.java
@@ -16,21 +16,22 @@
 class Solution {
     boolean flag = false;
     public boolean hasPathSum(TreeNode root, int targetSum) {
-        trav(root , targetSum);
+        if(root == null) return false;
+        dfs(root , targetSum , 0);
         return flag;
     }
 
-    private void trav(TreeNode root, int targetSum){
-        if (root == null) return ;
-        if(root.left == null && root.right == null){
-            if(targetSum == root.val){
-                flag = true;
-            }
+    private void dfs(TreeNode root, int targetSum , int currsum){
+        if(root == null) return;
+        currsum += root.val;
+
+        if(root.left == null && root.right == null && currsum == targetSum){
+            flag = true;
             return;
         }
 
-        trav(root.left , targetSum - root.val);
-        trav(root.right , targetSum - root.val);
 
+        dfs(root.left , targetSum , currsum);
+        dfs(root.right , targetSum , currsum);
     }
 }
