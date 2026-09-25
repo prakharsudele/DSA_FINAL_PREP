736f1e3f201f01b501566add446a69f6ab5530af
prakharsudele <prakharsudele36@gmail.com>
2026-09-25 11:48:11 +0530

    Time: 142 ms (43.64%) | Memory: 42.9 MB (75.54%) - LeetSync

--- Leetcode/Arrays-Hashing/79-word-search/word-search.java
+++ Leetcode/Arrays-Hashing/79-word-search/word-search.java
@@ -1,4 +1,5 @@
 class Solution {
+    //use dfs traversal + backtracking to visit all cells whenever found start of string run dfs from there to check if it can make full word.
     boolean flag = false;
     public boolean exist(char[][] board, String word) {
         int n = board.length;
@@ -6,7 +7,7 @@ class Solution {
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(board[i][j] == word.charAt(0)){
-                    find(board , word , i , j , n , m , 0);
+                    find(board , word , i , j , n , m , 0);//start of word run dfs from here.
                 }
             }
         }
@@ -16,18 +17,21 @@ class Solution {
     private void find(char[][] board, String word , int row , int col , int n , int m , int index){
         if(row < 0 || row >= n || col < 0 || col >= m || board[row][col] != word.charAt(index)) return;
 
-        if(index == word.length()-1){
+        if(index == word.length()-1){//reached at the end of word then we got our answer.
             flag = true;
             return;
         }
 
-        char temp = board[row][col];
+        char temp = board[row][col];//we will reuse same character multiple times hence we need to mark this as visited while backtracking we will change this back to normal if it's not our answer.
         board[row][col] = '#';
         find(board , word , row+1 , col , n , m , index+1);
         find(board , word , row-1 , col , n , m , index+1);
         find(board , word , row , col+1 , n , m , index+1);
         find(board , word , row , col-1 , n , m , index+1);
 
-        board[row][col] = temp;
+        board[row][col] = temp;//back to nornal answer not found yet.
     }
 }
+
+//TC --> O()
+//SC --> O()
