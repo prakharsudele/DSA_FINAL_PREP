82994ec6b1a5ff05f4e195effa2179d266382384
prakharsudele <prakharsudele36@gmail.com>
2026-09-26 09:27:37 +0530

    Time: 48 ms (21.72%) | Memory: 96 MB (31.67%) - LeetSync

--- Leetcode/Arrays-Hashing/1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.java
+++ Leetcode/Arrays-Hashing/1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.java
@@ -1,19 +1,19 @@
 class Solution {
     public String evaluate(String s, List<List<String>> knowledge) {
         StringBuilder sb = new StringBuilder();
-        HashMap<String , String> map = new HashMap<>();
+        HashMap<String , String> map = new HashMap<>();//map to store all key value pair so it can be accesed in O(1) time.
         for(List<String> it : knowledge) map.put(it.get(0) , it.get(1));
 
-        boolean inside = false;
-        String news = "";
+        boolean inside = false;//if we are inside brackets.
+        String news = "";//inside string building.
 
         for(int i=0;i<s.length();i++){
-            if(s.charAt(i) == '('){
+            if(s.charAt(i) == '('){//start bracket
                 inside = true;
                 continue;
             }
 
-            if(s.charAt(i) == ')'){
+            if(s.charAt(i) == ')'){//stop bracket and check condition to figure out answer.
                 if(!map.containsKey(news)) sb.append('?');
                 else sb.append(map.get(news));
                 news = "";
@@ -21,10 +21,13 @@ class Solution {
                 continue;
             }
 
-            if(inside) news += s.charAt(i);
-            else sb.append(s.charAt(i));
+            if(inside) news += s.charAt(i);//if inside build news string.
+            else sb.append(s.charAt(i));//if outside brackets add them normally
         }
 
         return sb.toString();
     }
 }
+
+//SC --> O(N)
+//TC --> O(N)
